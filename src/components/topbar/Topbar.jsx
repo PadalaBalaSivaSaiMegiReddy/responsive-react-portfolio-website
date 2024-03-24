92cadf193dc16fb208079fe37577690844b6c9d7
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-03-24 21:23:51 +0530

    Add MUI icons and menu functionality

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,19 +1,36 @@
+import { Email } from "@mui/icons-material";
 import "./Topbar.scss";
+import PersonIcon from "@mui/icons-material/Person";
+import MenuIcon from "@mui/icons-material/Menu";
 
-function Topbar() {
-  return <div  id="topbar" className="topbar">
-    <div className="wrapper">
-      <div className="left">
-        <a href="#intro" className="logo">megi.</a>
-        <div className="itemContainer">
-          
+function Topbar({menuOpen,setMenuOpen}) {
+
+  return (
+    <div id="topbar" className={menuOpen?`topbar active`:`topbar` }>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            megireddy.
+          </a>
+          <div className="itemContainer">
+            <PersonIcon className="icon" />
+            <span>+91 9959722693</span>
+          </div>
+          <div className="itemContainer">
+            <Email className="icon" />
+            <span>megireddy21@gmail.com</span>
+          </div>
+        </div>
+        <div className="right">
+          <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
         </div>
       </div>
-      <div className="right">
-        This is right
-      </div>
     </div>
-  </div>;
+  );
 }
 
 export default Topbar;
