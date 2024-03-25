ea3db7c88b9c81247e5b699978c0d965d952f3a9
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-03-25 12:02:32 +0530

    Add react-type-animation library and update Intro component

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,7 +1,46 @@
 import "./Intro.scss";
+import { TypeAnimation } from 'react-type-animation';
 
 function Intro() {
-  return <div  id="intro" className="intro">Intro</div>;
+ 
+  return <div  id="intro" className="intro">
+    <div className="left">
+      <div className="imgContainer">
+        <img src="../../src/assets/realman.png" alt="" />
+      </div>
+    </div>
+    <div className="right">
+      <div className="wrapper">
+        <h2>Hi There, I'm</h2>
+        <h1>Megi Reddy</h1>
+        <h3>Software </h3>
+        <div>
+  <TypeAnimation
+    sequence={[
+      500,
+      '', // initially rendered starting point
+      1000,
+      'Engineer',
+      1000,
+      'Developer',
+      1000,
+      'Designer',
+      500,
+      'Tester',
+      500,
+    ]}
+    speed={50}
+    style={{ fontSize: '30px',display:"inline",fontWeight:"bold",color:"crimson" }}
+    repeat={Infinity}
+  />
+</div>
+      </div>
+      <a href="#portfolio">
+        <img src="../../src/assets/down.png" alt="" />
+      </a>
+    </div>
+
+  </div>;
 }
 
 export default Intro;
