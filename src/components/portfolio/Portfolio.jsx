0a3ff79276ee62e0cbb0c383bb2085ba0ce4d134
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-03-25 12:25:23 +0530

    Update Intro and Portfolio components

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,42 +1,62 @@
 import "./Portfolio.scss";
 
 function Portfolio() {
-  return <div  id="portfolio" className="portfolio">
-    <h1>Portfolio</h1>
-    <ul>
-      <li className="active">Featured</li>
-      <li>Web App</li>
-      <li>Mobile App</li>
-      <li>Design</li>
-      <li>Branding</li>
-    </ul>
-    <div className="container">
-      <div className="item">
-        <img src="" alt="" />
-        <h3>Banking App</h3>
-      </div>
-      <div className="item">
-        <img src="" alt="" />
-        <h3>Banking App</h3>
-      </div>
-      <div className="item">
-        <img src="" alt="" />
-        <h3>Banking App</h3>
-      </div>
-      <div className="item">
-        <img src="" alt="" />
-        <h3>Banking App</h3>
-      </div>
-      <div className="item">
-        <img src="" alt="" />
-        <h3>Banking App</h3>
-      </div>
-      <div className="item">
-        <img src="" alt="" />
-        <h3>Banking App</h3>
+  return (
+    <div id="portfolio" className="portfolio">
+      <h1>Portfolio</h1>
+      <ul>
+        <li className="active">Featured</li>
+        <li>Web App</li>
+        <li>Mobile App</li>
+        <li>Design</li>
+        <li>Branding</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img
+            src="https://media.istockphoto.com/id/147732062/photo/a-up-close-view-of-the-white-pillared-entrance-of-a-bank.jpg?s=1024x1024&w=is&k=20&c=WzKL6hdOBhN76aV_s8zrCnhcnYyvvkcUdm2g_LQhzbw="
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://media.istockphoto.com/id/147732062/photo/a-up-close-view-of-the-white-pillared-entrance-of-a-bank.jpg?s=1024x1024&w=is&k=20&c=WzKL6hdOBhN76aV_s8zrCnhcnYyvvkcUdm2g_LQhzbw="
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://media.istockphoto.com/id/147732062/photo/a-up-close-view-of-the-white-pillared-entrance-of-a-bank.jpg?s=1024x1024&w=is&k=20&c=WzKL6hdOBhN76aV_s8zrCnhcnYyvvkcUdm2g_LQhzbw="
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://media.istockphoto.com/id/147732062/photo/a-up-close-view-of-the-white-pillared-entrance-of-a-bank.jpg?s=1024x1024&w=is&k=20&c=WzKL6hdOBhN76aV_s8zrCnhcnYyvvkcUdm2g_LQhzbw="
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://media.istockphoto.com/id/147732062/photo/a-up-close-view-of-the-white-pillared-entrance-of-a-bank.jpg?s=1024x1024&w=is&k=20&c=WzKL6hdOBhN76aV_s8zrCnhcnYyvvkcUdm2g_LQhzbw="
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://media.istockphoto.com/id/147732062/photo/a-up-close-view-of-the-white-pillared-entrance-of-a-bank.jpg?s=1024x1024&w=is&k=20&c=WzKL6hdOBhN76aV_s8zrCnhcnYyvvkcUdm2g_LQhzbw="
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
       </div>
     </div>
-  </div>
+  );
 }
 
 export default Portfolio;
