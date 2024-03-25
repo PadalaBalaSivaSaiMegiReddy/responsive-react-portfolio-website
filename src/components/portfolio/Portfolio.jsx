b2ba0105b0f6cf2124ef9579861e964f7534fd16
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-03-25 12:21:37 +0530

    Add portfolio layout and styling

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,42 @@
 import "./Portfolio.scss";
 
 function Portfolio() {
-  return <div  id="portfolio" className="portfolio">Portfolio</div>;
+  return <div  id="portfolio" className="portfolio">
+    <h1>Portfolio</h1>
+    <ul>
+      <li className="active">Featured</li>
+      <li>Web App</li>
+      <li>Mobile App</li>
+      <li>Design</li>
+      <li>Branding</li>
+    </ul>
+    <div className="container">
+      <div className="item">
+        <img src="" alt="" />
+        <h3>Banking App</h3>
+      </div>
+      <div className="item">
+        <img src="" alt="" />
+        <h3>Banking App</h3>
+      </div>
+      <div className="item">
+        <img src="" alt="" />
+        <h3>Banking App</h3>
+      </div>
+      <div className="item">
+        <img src="" alt="" />
+        <h3>Banking App</h3>
+      </div>
+      <div className="item">
+        <img src="" alt="" />
+        <h3>Banking App</h3>
+      </div>
+      <div className="item">
+        <img src="" alt="" />
+        <h3>Banking App</h3>
+      </div>
+    </div>
+  </div>
 }
 
 export default Portfolio;
